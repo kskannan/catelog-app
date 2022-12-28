729b021321af79462b9336ca14b4b80e2205d750
Senthamaraikannan <kannan8505@gmail.com>
2022-12-28 20:05:30 +0000

    first commit

---
+++ src/main/java/com/cate/app/catelogapp/CatelogAppApplication.java
@@ -0,0 +1,13 @@
+package com.cate.app.catelogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatelogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatelogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cate/app/catelogapp/CatelogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cate.app.catelogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatelogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
